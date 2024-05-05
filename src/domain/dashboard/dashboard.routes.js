954258cc1b0e88f24c1cbd823141994c02bd55d5
MarselTan23 <marchelino.535220107@stu.untar.ac.id>
2024-05-05 23:15:32 +0700

    initial commit

--- src/domain/dashboard/dashboard.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const DashboardController = require('./dashboard.dontrollers');
-const router = Router();
-
-router.get("/api/:categoryId", DashboardController.index);
-// Router Web
-router.get('/', DashboardController.v_index);
-
-module.exports = { dashboardRouter: router }
